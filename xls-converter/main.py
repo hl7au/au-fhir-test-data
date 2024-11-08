1c3438a516131e0d19a40fcc66fcea05c131e950
Ilya Beda <ir4y.ix@gmail.com>
2024-11-08 17:22:52 +1100

    Fix conversion for patient

--- xls-converter/main.py
+++ xls-converter/main.py
@@ -27,10 +27,20 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument('filename')
 
+def remove_time(data):
+    if not isinstance(data,str):
+        return data
+    parts = data.split(' ')
+    return parts[0]
 
 if __name__ == '__main__':
     args = parser.parse_args()
     xlsx = pd.ExcelFile(args.filename)
     for sheet in sheets:
-        df = xlsx.parse(sheet)
-        df.to_csv(f"./testdata-csv/AU Core Sample Data - {sheet}.csv",index = None , header=True, encoding='utf-8-sig')
+        df = xlsx.parse(sheet, dtype=str)
+        if sheet == 'Patient':
+            df['birthDate'] = df['birthDate'].apply(remove_time)
+
+        df.to_csv(f"./testdata-csv/AU Core Sample Data - {sheet}.csv",
+                  index = None , header=True,
+                  encoding='utf-8-sig')
