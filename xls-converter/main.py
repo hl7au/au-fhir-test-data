62f4b4433173b85fe3a9044b7203195c22bfa792
Ilya Beda <ir4y.ix@gmail.com>
2024-11-08 13:25:21 +1100

    WIP XLSX to cvs converter

---
+++ xls-converter/main.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import argparse
+
+sheets =  [
+    'Patient',
+    'Encounter',
+    'Condition',
+    'AllergyIntolerance',
+    'Immunization',
+    'Observation',
+    'Organization',
+    'HealthcareService',
+    'PractitionerRole',
+    'Practitioner',
+    'Procedure',
+    'Location',
+    'MedicationRequest',
+    'Medication',
+    'Specimen',
+    'RelatedPerson',
+    'ServiceRequest',
+];
+
+parser = argparse.ArgumentParser(
+    prog='Test Data converter',
+    description='Load XLSX file with test data and convert it to csvs',
+)
+parser.add_argument('filename')
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    xlsx = pd.ExcelFile(args.filename)
+    for sheet in sheets:
+        df = xlsx.parse(sheet)
+        df.to_csv(f"../testdata-csv/AU Core Sample Data - {sheet}.csv",index = None , header=True)
